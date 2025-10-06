96131ca72348a94090467fea168c9ff467246c5b
sornwanee <57429687+noonaza@users.noreply.github.com>
2025-10-06 13:33:10 +0700

    4

--- lib/util/tlconstant.dart
+++ lib/util/tlconstant.dart
@@ -9,10 +9,10 @@ class TlConstant {
   //workstepupdate
   // static const String syncApi = 'http://192.168.98.10:8081';
   static const String syncApi = 'https://uat-api.thonglorpet.com/smart-ward';
-//  static const String syncApi = 'https://e-api.thonglorpet.com/smart-ward';
+  // static const String syncApi = 'https://e-api.thonglorpet.com/smart-ward';
 
 //  static final String version = '1.2.3'; // userAdmin
-  static final String version = 'UAT V.2.0.0'; // userAdmin
+  static final String version = 'UAT V.2.0.1'; // userAdmin
 
   static int runID() => DateTime.now().millisecondsSinceEpoch;
   static String random() => Random().nextInt(999).toString().padLeft(3, '0');
